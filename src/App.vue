60fe6de8b1b085d2b466fb05a6e3c473ba114504
Serenajandra <serena.jandra@gmail.com>
2022-11-08 17:46:46 +0100

    creato header statico

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+  export default{
+    components: {
+      AppHeader
+    }
+  }
 
 </script>
 
 <template>
-
+<AppHeader />
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/mixins.scss" as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,57 @@
+<script>
+export default{
+   name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <div class="logo"><a href=""><img src="../assets/img/dc-logo.png" alt="dcLogo"></a></div>
+        <nav>
+            <ul>
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+                <li><a href="">link</a></li> 
+            </ul>
+        </nav>
+    </header>
+
+    </template>
+
+<style lang="scss" scoped>
+ @use "../styles/partials/mixins.scss" as *;
+ @use "../styles/general.scss" as *;
+
+header{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    background-color: aquamarine;
+    .logo{
+        width: 40%;
+        height: 100%;
+        text-align: center;
+
+        img{
+            width: 60px;
+            margin-top: 10px;
+            vertical-align: middle;
+        }
+    }
+}
+
+ul{
+    @include flex-center ("horizontal");
+}
+
+
+
+</style>
